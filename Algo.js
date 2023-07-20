ebb4a3a6506a4d947c11b42a68a877888b5a410a
Sam Aronow <72639228+SamAronow@users.noreply.github.com>
2023-07-20 09:28:20 -0400

    Add files via upload

--- Algo.js
+++ Algo.js
@@ -9,9 +9,9 @@ function getMagnitude(vector) {
  }
 
 //function to get updated file
-function reduce(file,angle_threshold,distance_threshold){
+function reduce(coords,angle_threshold,distance_threshold){
     //get coordinates from the geoJSON and then loop through and round to 6th decimal
-    let coords =  file.features[0].geometry.coordinates[0];
+    //let coords =  file.features[0].geometry.coordinates[0];
     let len = coords.length;
     let newcoords = new Array(len);
     for (let i =0; i<len; i++){
@@ -19,20 +19,22 @@ function reduce(file,angle_threshold,distance_threshold){
     }
 
     for (let i =0; i<len; i++){ 
-        for (let j = 0; j<2;j++){ 
-            newcoords[i][j] = round(coords[i][j],6);
-        }
+            newcoords[i][0] = coords[i][0];
+            newcoords[i][1] = coords[i][1];
     }
 
     let backward_vector = new Array(2);
     let forward_vector = new Array(2);
     let theta=0; 
 
+
     //always keep the first and last point so don't need to include it 
     //in the loop because loop only serves to eliminate target points
     for (var i = 1; i<newcoords.length-1; i++){
-        backward_vector = [newcoords[i-1][0]-newcoords[i][0],newcoords[i-1][1]-newcoords[i][1]];
-        forward_vector = [newcoords[i+1][0]-newcoords[i][0],newcoords[i+1][1]-newcoords[i][1]];
+        backward_vector = [(365214.666667 *Math.cos((Math.PI/180)*coords[i][1]))*(newcoords[i-1][0]-newcoords[i][0]),365214.666667 *(newcoords[i-1][1]-newcoords[i][1])];
+        forward_vector = [(365214.666667 * Math.cos((Math.PI/180)*coords[i][1]))*(newcoords[i+1][0]-newcoords[i][0]),365214.666667*(newcoords[i+1][1]-newcoords[i][1])];
+        //backward_vector = [newcoords[i-1][0]-newcoords[i][0],newcoords[i-1][1]-newcoords[i][1]];
+        //forward_vector = [newcoords[i+1][0]-newcoords[i][0],newcoords[i+1][1]-newcoords[i][1]];
         theta = Math.acos(((forward_vector[0]*backward_vector[0])+(forward_vector[1]*backward_vector[1]))/(getMagnitude(forward_vector)*getMagnitude(backward_vector)));
         if (getMagnitude(backward_vector)<distance_threshold && theta>angle_threshold){
             newcoords.splice(i,1);
@@ -47,10 +49,10 @@ function translate(x,y,coords){
     var newcoords = new Array(coords.length);
     for (var i =0; i<coords.length; i++){
         newcoords[i] = new Array(2);
-        newcoords[i][0]=coords[i][0]+(1*x);
-        newcoords[i][1]=coords[i][1]+(1*y);
+        newcoords[i][0]=coords[i][0]+(x/(Math.cos((Math.PI/180)*coords[i][1])*365214.666667));
+        newcoords[i][1]=coords[i][1]+(y/365214.666667);
     }
-    return newcoords;
+    return roundCoords(newcoords,6);
 }
 
 //helper function to get the index of a given id
@@ -63,4 +65,56 @@ function search(id){
     return -1;
 }
 
+function roundCoords(coords,precision){
+    var newCoords = new Array(0)
+    var x;
+    var y;
+    for (var i=0; i<coords.length; i++){
+        x = coords[i][0];
+        y= coords[i][1]
+        if (needsRounding(x,precision)){
+            if (needsRounding(y,precision)){
+                newCoords.push([round(x,precision),round(y,precision)])
+            }
+            else{
+                newCoords.push([round(coords[i][0],precision),y])
+            }
+        }
+        else{
+            if (needsRounding(y,precision)){
+                newCoords.push([x,round(y,precision)])
+            }
+            else{
+                newCoords.push([x,y])
+            }
+        }
+    }
+    return newCoords;
+}
+
+
+function needsRounding(number,precision){
+    var val = number.toString();
+    if (val.indexOf(".")==-1){
+        return false;
+    }
+    val = val.substring(val.indexOf(".")+1)
+    if (val.length>precision){
+        return true;
+    }
+    return false;
+}
+
+function get2DIndex(array,value){
+    for (var i=0; i<array.length; i++){
+        if (array[i][0]==value[0] && array[i][1]==value[1]){
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+
 window.round = round;
+window.getMagnitude = getMagnitude;
--- MapFunctions.js
+++ MapFunctions.js
@@ -1,3 +1,12 @@
+//adds all the trails to the map and the arrays
+function setUpTrails(map){
+    var coords = window.keezer.features[0].geometry.coordinates[0];
+    addlay(roundCoords(coords,6),map,"keezer_d--_a--_tx--_ty--_cblue","blue");
+    //addlay(window.twine.features[0].geometry.coordinates[0],map,"twine_d--_a--_tx--_ty--_c#00f","#00f");
+    addAllPoints(roundCoords(coords,6),map);
+    document.getElementById("datatable").rows[1].cells[9].querySelector("button").textContent = "Unselect";
+}
+
 //adds a new layer with parametized conditions to the layer array
 function createLayerClass(coords,map,name,color){
     return {
@@ -26,6 +35,186 @@ function createLayerClass(coords,map,name,color){
     }
 }
 
+//adds a layer to the map by adding it to the layer array then to the map
+//then adds the toggle button to the toggle array
+function addlay(coords,map,name,color){
+    window.layers.push(createLayerClass(coords,map,name,color));
+    window.removedPoints.push(new Array(0));
+    window.movedPoints.push(new Array(0));
+    addDataToTable(name);
+    map.addLayer(window.layers[window.layers.length-1]);
+}
+
+//coord[0]+","+coord[1]
+function addPoint(coord,map,color){
+    var pt = {
+        id: coord[0]+","+coord[1],
+        type: 'circle',
+        source: {
+          type: 'geojson',
+          data: {
+            type: 'FeatureCollection',
+            features: [
+              {
+                type: 'Feature',
+                geometry: {
+                  type: 'Point',
+                  coordinates: coord // Set the coordinates of your point
+                },
+                properties: {
+                  // Add any additional properties for your point
+                  name: coord[0]+","+coord[1]
+                }
+              }
+            ]
+          }
+        },
+        paint: {
+            'circle-radius': [
+              'interpolate',
+              ['linear'],
+              ['zoom'],
+              15, .5, 
+              16, 1,
+              17.5,2.5
+            ],
+            'circle-color': color
+          },
+          interactive: true
+      }
+      map.addLayer(pt);
+    }
+
+    //function to remove a point
+function removePoint(point){
+    var coords = window.selected.source.data.geometry.coordinates;
+    //console.log(point)
+    //go through every point in the selected line's array and remove it if it matches the given point
+    removedPoints[window.selectedIndex].push(point);
+    var i =get2DIndex(coords,point);
+          map.removeLayer(coords[i][0]+","+coords[i][1]);
+          map.removeSource(coords[i][0]+","+coords[i][1]);
+          coords.splice(i,1);
+          RestoreLog[selectedIndex].push([time,coords])
+          
+          //reassing the selected line with the new array without the point
+          window.selected.source.data.geometry.coordinates = coords;
+          map.removeLayer(window.selected.id);
+          map.removeSource(window.selected.id);
+          map.addLayer(window.selected);
+}
+
+    function addClickPoint(coord,map,color){
+        var pt = {
+            id: "id",
+            type: 'circle',
+            source: {
+              type: 'geojson',
+              data: {
+                type: 'FeatureCollection',
+                features: [
+                  {
+                    type: 'Feature',
+                    geometry: {
+                      type: 'Point',
+                      coordinates: coord // Set the coordinates of your point
+                    },
+                    properties: {
+                      // Add any additional properties for your point
+                      name: coord[0]+","+coord[1]
+                    }
+                  }
+                ]
+              }
+            },
+            paint: {
+                'circle-radius': [
+                  'interpolate',
+                  ['linear'],
+                  ['zoom'],
+                  15, .5, 
+                  16, 1,
+                  17.5,2.5
+                ],
+                'circle-color': color
+              },
+              interactive: true
+          }
+          map.addLayer(pt);
+        }
+
+
+        function addAllPoints(coords,map){
+            for(var i=0; i<coords.length;i++){
+                addPoint(coords[i],map,"red");
+            }
+        }
+        
+        function removeAllPoints(coords,map){
+            for (var i =0; i<coords.length; i++){
+            map.removeLayer(coords[i][0]+","+coords[i][1]);
+            map.removeSource(coords[i][0]+","+coords[i][1]);
+            }
+        }
+
+function removeAllSelectedPoints(){
+    if (window.selectedPoints.length==0){
+        printError("No selected Points")
+        return
+    }
+    var coords = window.selected.source.data.geometry.coordinates;
+    var point;
+    for (var j =0; j<window.selectedPoints.length; j++){
+    point = window.selectedPoints[j];
+      //go through every point in the selected line's array and remove it if it matches the given point
+      removedPoints[window.selectedIndex].push(point);
+      var i = get2DIndex(coords,point)
+            map.removeLayer(coords[i][0]+","+coords[i][1]);
+            map.removeSource(coords[i][0]+","+coords[i][1]);
+            coords.splice(i,1);
+      window.selectedPoints.splice(j,1);
+      j--;
+    }
+    //reassing the selected line with the new array without the point
+    window.selected.source.data.geometry.coordinates = coords;
+    map.removeLayer(window.selected.id);
+    map.removeSource(window.selected.id);
+    map.addLayer(window.selected);
+    layers[window.selectedIndex] = selected;
+  }
+
+
+  function findClosestPoint(coords,point){
+    var closestPt = coords[0];
+    var closestVec =[Math.abs(365214.666667*Math.cos((Math.PI/180)*coords[0][1])*(coords[0][0]-point[0])),Math.abs(365214.666667 *(coords[0][1]-point[1]))];
+    var curVec;
+    var closestI=0;
+    //console.log(coords[0]);
+    /*
+    console.log(closestVec);
+    console.log(window.getMagnitude(closestVec));
+    console.log("");*/
+    for (var i =1; i<coords.length; i++){
+        curVec =[Math.abs(365214.666667*Math.cos((Math.PI/180)*coords[i][1])*(coords[i][0]-point[0])),Math.abs(365214.666667 *(coords[i][1]-point[1]))];
+        /*console.log(i);
+        console.log(curVec);
+        console.log(window.getMagnitude(curVec));*/
+        if (window.getMagnitude(curVec)<window.getMagnitude(closestVec)){
+            closestVec = curVec;
+            closestI=i;
+            closestPt = coords[i];
+        }
+        /*
+        console.log(closestVec);
+        console.log("");*/
+    }
+    //console.log(closestI);
+    return closestPt;
+}
+
+
+
+
 //given an id it returns an array of distance threshold, angle threshold etc..
 function getInfo(name){
     var info = new Array(7);
@@ -51,8 +240,8 @@ function addDataToTable(name){
     var table = document.getElementById("datatable");
     var newRow = table.insertRow();
 
-    var buttons = new Array(3);
-    for (var i =0; i<4; i++){
+    var buttons = new Array(4);
+    for (var i =0; i<5; i++){
         buttons[i] = document.createElement("button");
     }
 
@@ -72,8 +261,12 @@ function addDataToTable(name){
     buttons[3].textContent = "Copy";
     buttons[3].addEventListener("click",copy);
 
+    buttons[4].id = name+"sel";
+    buttons[4].textContent = "Select";
+    buttons[4].addEventListener("click",changeSelected);
+
     var info = getInfo(name);
-    for (var i = 0; i < 9; i++) {
+    for (var i = 0; i < 10; i++) {
         newRow.insertCell();
         if (i<5){
             newRow.cells[i].innerHTML = info[i];
@@ -86,6 +279,7 @@ function addDataToTable(name){
       }
  }
 
+ //checks whether this name already exists
  function validEnter(name){
     var curInfo = getInfo(name);
     var tempInfo;
@@ -107,17 +301,68 @@ function addDataToTable(name){
 
 
 
-//adds a layer to the map by adding it to the layer array then to the map
-//then adds the toggle button to the toggle array
-function addlay(coords,map,name,color){
-    window.layers.push(createLayerClass(coords,map,name,color));
-    addDataToTable(name);
-    map.addLayer(window.layers[window.layers.length-1]);
+//function to give an array with the values of the textfields
+function gatherFields(){
+    //ensure valid entries
+    if (document.getElementById("distance").value=="" || isNaN(document.getElementById("distance").value)){
+        printError("Distance Threshold: is not valid");
+        return new Array(0);
+    }
+    if (document.getElementById("angle").value=="" || isNaN(document.getElementById("angle").value)){
+        printError("Angle Threshold: is not valid");
+        return new Array(0);
+    }
+    if (document.getElementById("translationX").value=="" || isNaN(document.getElementById("translationX").value)){
+        printError("translation x: is not valid");
+        return new Array(0);
+    }
+    if (document.getElementById("translationY").value==""  || isNaN(document.getElementById("translationY").value)){
+        printError("translation x: is not valid");
+        return new Array(0);
+    }
+    var fields = new Array(6);
+    fields[0] = document.getElementById('distance').value;
+    fields[1] = document.getElementById('angle').value;
+    fields[2] = document.getElementById('translationX').value;
+    fields[3] = document.getElementById('translationY').value;
+    fields[4] = document.getElementById('color').value;
+    fields[5] = document.getElementById("file").value;
+    var drop = document.getElementById("file");
+    var fileName = drop.options[drop.selectedIndex].textContent;
+    fields[6] = fileName+"_d"+fields[0]+"_a"+fields[1]+"_tx"+fields[2]+"_ty"+fields[3]+"_c"+fields[4];
+    return fields;
 }
 
-//adds all the trails to the map and the arrays
-function setUpTrails(map){
-    addlay(window.keezer.features[0].geometry.coordinates[0],map,"keezer_d--_a--_tx--_ty--_cblue","blue");
-    //addlay(window.twine.features[0].geometry.coordinates[0],map,"twine_d--_a--_tx--_ty--_c#00f","#00f");
+function removeShowPoint(map){
+    if (window.showPoint!=null){
+        map.setPaintProperty(window.showPoint[0]+","+window.showPoint[1], 'circle-radius', [
+                      'interpolate',
+                      ['linear'],
+                      ['zoom'],
+                      15, .5, 
+                      16, 1,
+                      17.5,2.5
+                    ]);
+        window.showPoint = null;
+        document.getElementById('lastPoint').style.display = 'none';
+      }
+}
 
+function removeMovePoint(map){
+    if (window.movePoint!=null){
+        map.setPaintProperty(window.movePoint[0]+","+window.movePoint[1], 'circle-radius', [
+                      'interpolate',
+                      ['linear'],
+                      ['zoom'],
+                      15, .5, 
+                      16, 1,
+                      17.5,2.5
+                    ]);
+        window.movePoint = null;
+      }
+}
+
+function moveInd(point){
+    var array = window.movedPoints[selectedIndex];
+    return get2DIndex(window.movedPoints[selectedIndex],point);
 }
