0f68f07629216de527e0ea00bcd7c05e79faa176
Sam Aronow <72639228+SamAronow@users.noreply.github.com>
2023-07-28 11:55:18 -0400

    Add files via upload

--- MapFunctions.js
+++ MapFunctions.js
@@ -1,7 +1,7 @@
 //adds all the trails to the map and the arrays
 function setUpTrails(map){
     var coords = window.keezer.features[0].geometry.coordinates[0];
-    addlay(roundCoords(coords,6),map,"keezer_d--_a--_tx--_ty--_cblue","blue");
+    addlay(roundCoords(coords,6),map,"keezer_d--_a--_tx--_ty--_cblue_ll","blue");
     //addlay(window.twine.features[0].geometry.coordinates[0],map,"twine_d--_a--_tx--_ty--_c#00f","#00f");
     addAllPoints(roundCoords(coords,6),map);
     document.getElementById("datatable").rows[1].cells[9].querySelector("button").textContent = "Unselect";
@@ -32,6 +32,7 @@ function createLayerClass(coords,map,name,color){
             "line-color": color,
             "line-width": 3
         }
+        
     }
 }
 
@@ -39,16 +40,21 @@ function createLayerClass(coords,map,name,color){
 //then adds the toggle button to the toggle array
 function addlay(coords,map,name,color){
     window.layers.push(createLayerClass(coords,map,name,color));
-    window.removedPoints.push(new Array(0));
-    window.movedPoints.push(new Array(0));
-    addDataToTable(name);
+    window.states.push(new Array(0));
+    var newCoords = new Array(0);
+    for (var i =0; i<coords.length; i++){
+        newCoords.push(coords[i])
+    }
+    window.states[window.states.length-1].push(newCoords);
+    addDataToTable(name,map);
     map.addLayer(window.layers[window.layers.length-1]);
+    //window.states[window.states.length-1] = window.layers[window.layers.length-1].source.data.geometry.coordinates;
 }
 
 //coord[0]+","+coord[1]
-function addPoint(coord,map,color){
+function addPoint(coord,map,color,index){
     var pt = {
-        id: coord[0]+","+coord[1],
+        id: coord[0]+","+coord[1]+","+index,
         type: 'circle',
         source: {
           type: 'geojson',
@@ -81,96 +87,73 @@ function addPoint(coord,map,color){
             'circle-color': color
           },
           interactive: true
-      }
+        }
       map.addLayer(pt);
     }
 
     //function to remove a point
 function removePoint(point){
-    var coords = window.selected.source.data.geometry.coordinates;
-    //console.log(point)
+       var coords = new Array(0);
+       for (var i=0; i<window.layers[window.selectedIndex].source.data.geometry.coordinates.length; i++){
+        coords.push(window.layers[window.selectedIndex].source.data.geometry.coordinates[i])
+       }
     //go through every point in the selected line's array and remove it if it matches the given point
-    removedPoints[window.selectedIndex].push(point);
+
     var i =get2DIndex(coords,point);
-          map.removeLayer(coords[i][0]+","+coords[i][1]);
-          map.removeSource(coords[i][0]+","+coords[i][1]);
+    removeAllPoints(coords,map)
           coords.splice(i,1);
-          RestoreLog[selectedIndex].push([time,coords])
+         // RestoreLog[selectedIndex].push([time,coords])
           
           //reassing the selected line with the new array without the point
-          window.selected.source.data.geometry.coordinates = coords;
+          window.layers[window.selectedIndex].source.data.geometry.coordinates = coords;
           map.removeLayer(window.selected.id);
           map.removeSource(window.selected.id);
           map.addLayer(window.selected);
+          addAllPoints(coords,map)
 }
 
-    function addClickPoint(coord,map,color){
-        var pt = {
-            id: "id",
-            type: 'circle',
-            source: {
-              type: 'geojson',
-              data: {
-                type: 'FeatureCollection',
-                features: [
-                  {
-                    type: 'Feature',
-                    geometry: {
-                      type: 'Point',
-                      coordinates: coord // Set the coordinates of your point
-                    },
-                    properties: {
-                      // Add any additional properties for your point
-                      name: coord[0]+","+coord[1]
-                    }
-                  }
-                ]
-              }
-            },
-            paint: {
-                'circle-radius': [
-                  'interpolate',
-                  ['linear'],
-                  ['zoom'],
-                  15, .5, 
-                  16, 1,
-                  17.5,2.5
-                ],
-                'circle-color': color
-              },
-              interactive: true
-          }
-          map.addLayer(pt);
-        }
-
-
-        function addAllPoints(coords,map){
+function addAllPoints(coords,map){
+         if (window.selected!=null){
+            if(getInfo(selected.id)[7]=="red"){
+                for(var i=0; i<coords.length;i++){
+                    addPoint(coords[i],map,"black",i);
+             }
+            }
+            else{
+                for(var i=0; i<coords.length;i++){
+                    addPoint(coords[i],map,"red",i);
+             }
+            }
+         }
+         else{
             for(var i=0; i<coords.length;i++){
-                addPoint(coords[i],map,"red");
-            }
-        }
+                addPoint(coords[i],map,"red",i);
+         }
+         }
+    }
         
-        function removeAllPoints(coords,map){
-            for (var i =0; i<coords.length; i++){
-            map.removeLayer(coords[i][0]+","+coords[i][1]);
-            map.removeSource(coords[i][0]+","+coords[i][1]);
-            }
+ function removeAllPoints(coords,map){
+        for (var i =0; i<coords.length; i++){
+         map.removeLayer(coords[i][0]+","+coords[i][1]+","+i);
+           map.removeSource(coords[i][0]+","+coords[i][1]+"," +i);
+        }
         }
 
 function removeAllSelectedPoints(){
     if (window.selectedPoints.length==0){
         printError("No selected Points")
-        return
+        return;
     }
-    var coords = window.selected.source.data.geometry.coordinates;
+    var coords = new Array(0);
+    for (var i=0; i<window.layers[window.selectedIndex].source.data.geometry.coordinates.length; i++){
+     coords.push(window.layers[window.selectedIndex].source.data.geometry.coordinates[i])
+    }
     var point;
+    removeAllPoints(coords,map)
     for (var j =0; j<window.selectedPoints.length; j++){
     point = window.selectedPoints[j];
       //go through every point in the selected line's array and remove it if it matches the given point
-      removedPoints[window.selectedIndex].push(point);
       var i = get2DIndex(coords,point)
-            map.removeLayer(coords[i][0]+","+coords[i][1]);
-            map.removeSource(coords[i][0]+","+coords[i][1]);
             coords.splice(i,1);
       window.selectedPoints.splice(j,1);
       j--;
@@ -180,7 +163,11 @@ function removeAllSelectedPoints(){
     map.removeLayer(window.selected.id);
     map.removeSource(window.selected.id);
     map.addLayer(window.selected);
+    addAllPoints(coords,map)
     layers[window.selectedIndex] = selected;
+    addState();
+    editPercentage()
+    editLength()
   }
 
 
@@ -189,26 +176,17 @@ function removeAllSelectedPoints(){
     var closestVec =[Math.abs(365214.666667*Math.cos((Math.PI/180)*coords[0][1])*(coords[0][0]-point[0])),Math.abs(365214.666667 *(coords[0][1]-point[1]))];
     var curVec;
     var closestI=0;
-    //console.log(coords[0]);
-    /*
-    console.log(closestVec);
-    console.log(window.getMagnitude(closestVec));
-    console.log("");*/
     for (var i =1; i<coords.length; i++){
         curVec =[Math.abs(365214.666667*Math.cos((Math.PI/180)*coords[i][1])*(coords[i][0]-point[0])),Math.abs(365214.666667 *(coords[i][1]-point[1]))];
-        /*console.log(i);
-        console.log(curVec);
-        console.log(window.getMagnitude(curVec));*/
         if (window.getMagnitude(curVec)<window.getMagnitude(closestVec)){
             closestVec = curVec;
             closestI=i;
             closestPt = coords[i];
         }
-        /*
-        console.log(closestVec);
-        console.log("");*/
     }
-    //console.log(closestI);
+    if (window.getMagnitude(closestVec)>15){
+        return null;
+    }
     return closestPt;
 }
 
@@ -217,12 +195,12 @@ function removeAllSelectedPoints(){
 
 //given an id it returns an array of distance threshold, angle threshold etc..
 function getInfo(name){
-    var info = new Array(7);
+    var info = new Array(8);
     info[1] = name.substring(0,name.indexOf("_d"));
     info[2] = name.substring(name.indexOf("_d")+2,name.indexOf("_a"));
     info[3] = name.substring(name.indexOf("_a")+2,name.indexOf("_tx"));
     info[4] = name.substring(name.indexOf("_tx")+3,name.indexOf("_ty"))+"/"+name.substring(name.indexOf("_ty")+3,name.indexOf("_c"));
-    info[5] = name.substring(name.indexOf("_c")+2);
+    info[7] = name.substring(name.indexOf("_c")+2,name.indexOf("_l"));
     var dropdown = document.getElementById("file");
     Array.from(dropdown.options).forEach(function(element) {
         if (element.textContent == info[1]){
@@ -232,7 +210,21 @@ function getInfo(name){
             catch(error){}
         }
       });
-    info[0] = window.round((window.layers[window.layers.length-1].source.data.geometry.coordinates.length*100.0)/(info[6].features[0].geometry.coordinates[0].length),2)+"%";
+      var linestring = {
+        "type": "Feature",
+        "geometry": {
+            "type": "LineString",
+            "coordinates": info[6].features[0].geometry.coordinates[0]
+        }
+    };
+    info[5] = round(turf.lineDistance(linestring)*3280.84/5280,3)+"mi";
+      try{
+        info[0] = window.round((window.layers[window.layers.length-1].source.data.geometry.coordinates.length*100.0)/(info[6].features[0].geometry.coordinates[0].length),2)+"%";
+      }
+      catch(error){
+
+      }
+    
     return info;
 }
 
@@ -241,7 +233,7 @@ function addDataToTable(name){
     var newRow = table.insertRow();
 
     var buttons = new Array(4);
-    for (var i =0; i<5; i++){
+    for (var i =0; i<4; i++){
         buttons[i] = document.createElement("button");
     }
 
@@ -253,28 +245,27 @@ function addDataToTable(name){
     buttons[1].textContent = "Remove";
     buttons[1].addEventListener("click",remove);
 
-    buttons[2].id = name+"edit";
-    buttons[2].textContent = "Edit";
-    buttons[2].addEventListener("click",promptChange);
+    buttons[2].id = name+"copy";
+    buttons[2].textContent = "Copy";
+    buttons[2].addEventListener("click",copy);
 
-    buttons[3].id = name+"copy";
-    buttons[3].textContent = "Copy";
-    buttons[3].addEventListener("click",copy);
-
-    buttons[4].id = name+"sel";
-    buttons[4].textContent = "Select";
-    buttons[4].addEventListener("click",changeSelected);
+    buttons[3].id = name+"sel";
+    buttons[3].textContent = "Select";
+    buttons[3].addEventListener("click",changeSelected);
 
     var info = getInfo(name);
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < 11; i++) {
+        if (i==6){
+            continue;
+        }
         newRow.insertCell();
-        if (i<5){
+        if (i<6){
             newRow.cells[i].innerHTML = info[i];
-            newRow.cells[i].style.color = info[5];
+            newRow.cells[i].style.color = info[7];
         }
         else{
-            newRow.cells[i].appendChild(buttons[i-5]);
-            buttons[i-5].style.color = info[5];
+            newRow.cells[i-1].appendChild(buttons[i-7]);
+            buttons[i-7].style.color = info[7];
         }
       }
  }
@@ -287,8 +278,14 @@ function addDataToTable(name){
         for (var i =0; i<window.layers.length; i++){
             valid = false;
             tempInfo = getInfo(window.layers[i].id);
-            for (var j =0; j<curInfo.length-1; j++){
+           
+             //   console.log(tempInfo)
+            for (var j =0; j<curInfo.length; j++){
                 if (curInfo[j]!=tempInfo[j]){
+                    if (j==6){
+                        continue;
+                    }
+                    
                     valid = true;
                 }
             }
@@ -326,16 +323,18 @@ function gatherFields(){
     fields[2] = document.getElementById('translationX').value;
     fields[3] = document.getElementById('translationY').value;
     fields[4] = document.getElementById('color').value;
-    fields[5] = document.getElementById("file").value;
+    fields[5] = null;
+    fields[6] = document.getElementById("file").value;
     var drop = document.getElementById("file");
     var fileName = drop.options[drop.selectedIndex].textContent;
-    fields[6] = fileName+"_d"+fields[0]+"_a"+fields[1]+"_tx"+fields[2]+"_ty"+fields[3]+"_c"+fields[4];
+    fields[7] = fileName+"_d"+fields[0]+"_a"+fields[1]+"_tx"+fields[2]+"_ty"+fields[3]+"_c"+fields[4]+"_ll";
     return fields;
 }
 
 function removeShowPoint(map){
     if (window.showPoint!=null){
-        map.setPaintProperty(window.showPoint[0]+","+window.showPoint[1], 'circle-radius', [
+        var i = get2DIndex(window.layers[window.selectedIndex].source.data.geometry.coordinates,showPoint)
+        map.setPaintProperty(window.showPoint[0]+","+window.showPoint[1]+","+i, 'circle-radius', [
                       'interpolate',
                       ['linear'],
                       ['zoom'],
@@ -350,7 +349,9 @@ function removeShowPoint(map){
 
 function removeMovePoint(map){
     if (window.movePoint!=null){
-        map.setPaintProperty(window.movePoint[0]+","+window.movePoint[1], 'circle-radius', [
+        var i = get2DIndex(window.layers[window.selectedIndex].source.data.geometry.coordinates,movePoint)
+
+        map.setPaintProperty(window.movePoint[0]+","+window.movePoint[1]+","+i, 'circle-radius', [
                       'interpolate',
                       ['linear'],
                       ['zoom'],
@@ -362,7 +363,51 @@ function removeMovePoint(map){
       }
 }
 
-function moveInd(point){
-    var array = window.movedPoints[selectedIndex];
-    return get2DIndex(window.movedPoints[selectedIndex],point);
+function removeAddBeforePoint(map){
+    if (window.addBeforePoint!=null){
+    
+        var i = get2DIndex(window.layers[window.selectedIndex].source.data.geometry.coordinates,addBeforePoint)
+        map.setPaintProperty(window.addBeforePoint[0]+","+window.addBeforePoint[1]+","+i, 'circle-radius', [
+                      'interpolate',
+                      ['linear'],
+                      ['zoom'],
+                      15, .5, 
+                      16, 1,
+                      17.5,2.5
+                    ]);
+        window.addBeforePoint = null;
+        document.getElementById('lastPoint').style.display = 'none';
+      }
+}
+
+var restored = false;
+
+function addState(){
+  // console.log(window.states[selectedIndex])
+    var newCoords = new Array(0);
+    for (var i =0; i<window.layers[window.selectedIndex].source.data.geometry.coordinates.length; i++){
+        newCoords.push(window.layers[window.selectedIndex].source.data.geometry.coordinates[i])
+    }
+        window.states[selectedIndex].push(newCoords);
+
+    newCoords= null;
+    updateVersionDisplay()
+}
+
+function editPercentage(){
+    var info = getInfo(selected.id)
+    
+  document.getElementById("datatable").rows[window.selectedIndex+1].cells[0].innerHTML = round((window.layers[window.selectedIndex].source.data.geometry.coordinates.length*100.0)/(info[6].features[0].geometry.coordinates[0].length),2)+"%";
+  
+}
+
+function editLength(){
+      var linestring = {
+        "type": "Feature",
+        "geometry": {
+            "type": "LineString",
+            "coordinates": window.layers[window.selectedIndex].source.data.geometry.coordinates
+        }
+    };
+    document.getElementById("datatable").rows[window.selectedIndex+1].cells[5].innerHTML = round(turf.lineDistance(linestring)*3280.84/5280,3)+"mi";
 }
